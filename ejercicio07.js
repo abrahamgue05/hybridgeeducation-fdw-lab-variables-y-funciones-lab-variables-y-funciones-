920793863f3dae1f21902d45af2ed2f59608147c
Abraham Guerrero <143229944+abrahamgue05@users.noreply.github.com>
2025-11-03 20:04:37 -0600

    Implement esPar function to check for even numbers

--- ejercicio07.js
+++ ejercicio07.js
@@ -2,7 +2,11 @@
 // Declara una función llamada "esPar" que acepte un parámetro "numero" y retorne verdadero si el número es par, de lo contrario falso
 
 //escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "esPar"
-
+function esPar(numero) {
+  // El operador de módulo (%) da el residuo de una división.
+  // Si el residuo de dividir entre 2 es 0, es par.
+  return numero % 2 === 0;
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
