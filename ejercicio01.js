1ab15ebc11052aba4805c105adec89b6cb26976d
Abraham Guerrero <143229944+abrahamgue05@users.noreply.github.com>
2025-11-03 20:09:11 -0600

    Update ejercicio01.js

--- ejercicio01.js
+++ ejercicio01.js
@@ -2,7 +2,8 @@
 // Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
 
 function obtenerNombre() {
-    // Escribe tu código aquí
+  // Obviamente, cambia "Mundo" por tu nombre.
+  return "Mundo";
 }
 
 // No modificar.
